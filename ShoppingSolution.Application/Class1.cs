dd40e10027da9936c069ed8d38973127913bc30f
DaoCuong <cuong.263@gmail.com>
2020-07-26 16:12:14 +0700

    create solution structure

---
+++ ShoppingSolution.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ShoppingSolution.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ ShoppingSolution.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ShoppingSolution.Data
+{
+    public class Class1
+    {
+    }
+}
